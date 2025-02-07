21cc42341d906b2487e868b31402059012054dc4
Carsonmilesfan <carsonaholmes@gmail.com>
2025-02-07 10:57:11 -0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -10,6 +10,17 @@ def title():
     # Author: Copper
     # Modified:
     # title
+    print("Welcome To The Geometry/Graphing Calculator!")
+    print("This Calculator Has Eight Functions")
+    print("Enter \"1\" To Calculate The Area Of A Rectangle")
+    print("Enter \"2\" To Calculate The Volume Of A Rectangular Prism")
+    print("Enter \"3\" To Calculate The Area Of A Triangle")
+    print("Enter \"4\" To Calculate The Volume Of A Triangular Prism")
+    print("Enter \"5\" To Calculate The Area Of A Circle")
+    print("Enter \"6\" To Calculate The Volume Of A Sphere")
+    print("Enter \"7\" To Calculate The Slope Of A Graph At A Specific Point")
+    print("Enter \"8\" To Calculate The Area Under A Function At An Interval")
+    MILESSANDERS = input("What Would You Like To Do? => ")
     return None
 
 def instructions():
