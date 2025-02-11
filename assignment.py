f095abb48cae1f0c6cafa04719ff016ad8e273d2
Carsonmilesfan <carsonaholmes@gmail.com>
2025-02-11 10:37:27 -0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,7 +2,6 @@
 # Volume Calculator
 # Feel free to rename your variables
 
-
 def title():
     # Will display a title screen
     # input parameters: none needed
@@ -30,22 +29,25 @@ def instructions():
     print("Enter \"7\" To Calculate The Slope Of A Graph At A Specific Point")
     print("Enter \"8\" To Calculate The Area Under A Function At An Interval")
     MILESSANDERS = input("What Would You Like To Do? => ")
-    if MILESSANDERS == 1:
-        rectangle()
-    elif MILESSANDERS == 2:
-        rectangularprism()
-    elif MILESSANDERS == 3:
-        triangle()
-    elif MILESSANDERS == 4:
-        triangularprism()
-    elif MILESSANDERS == 5:
-        circle()
-    elif MILESSANDERS == 6:
-        sphere()
-    elif MILESSANDERS == 7:
-        slope()
-    elif MILESSANDERS == 8:
-        accumulation()
+    try:
+        if MILESSANDERS == 1:
+            rectangle()
+        elif MILESSANDERS == 2:
+            rectangularprism()
+        elif MILESSANDERS == 3:
+            triangle()
+        elif MILESSANDERS == 4:
+            triangularprism()
+        elif MILESSANDERS == 5:
+            circle()
+        elif MILESSANDERS == 6:
+            sphere()
+        elif MILESSANDERS == 7:
+            slope()
+        elif MILESSANDERS == 8:
+            accumulation()
+    except:
+        print("invalid command")
     return None
 
 
@@ -56,12 +58,18 @@ def main():
     You will need to include a while loop to keep repeating the commands until
     the user chooses to exit
     """
+    
     title()
     while True:
         # keep giving options to choose menu options until they choose to exit
         pass
 
 def rectangle():
+
+    milesinstructions = int(input("What Is The Width Of Your Rectangle? => "))
+    jericksinstructions = int(input("What Is The Length Of Your Rectangle? => "))
+    hotasthesumma = milesinstructions * jericksinstructions
+    return hotasthesumma
 
 def rectangularprism():
 
